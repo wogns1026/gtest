e6496908d007e1f9f89e3c64317e5c51a9436092
wogns1026 <sksmsljh1026@naver.com>
2019-05-13 16:12:56 +0900

    Update test.cpp

--- gtest/test.cpp
+++ gtest/test.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 
-TEST(addFuncTest, Normal) {
+TEST(pchTest, addFuncTest) {
 	EXPECT_EQ(2, addFunc(1, 1));
 	// 사용자 지정 오류 메세지 제공하는 법
 	EXPECT_EQ(3, addFunc(1, 1)) << "1 + 1 is not 3";
@@ -11,22 +11,22 @@ TEST(addFuncTest, Normal) {
 	EXPECT_GE(2, addFunc(1, 1));
 }
 
-TEST(subFuncTest, Normal) {
+TEST(pchTest, subFuncTest) {
 	EXPECT_EQ(3, subFunc(5, 2));
 }
 
-TEST(mulFuncTest, Normal) {
+TEST(pchTest, mulFuncTest) {
 	EXPECT_EQ(6, mulFunc(2, 3));
 }
 
-TEST(divFuncTest, Normal) {
+TEST(pchTest, divFuncTest) {
 	EXPECT_EQ(3, divFunc(6, 2));
 }
 
-TEST(factorialFuncTest, Normal) {
+TEST(pchTest, factorialFuncTest) {
 	EXPECT_EQ(6, factorialFunc(3));
 }
 
-TEST(combinationFuncTest, Normal) {
+TEST(pchTest, combinationFuncTest) {
 	EXPECT_EQ(35, combinationFunc(7, 4));
 }
